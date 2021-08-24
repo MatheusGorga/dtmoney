bd066a5293747ecac4c97e8c560a3dcdf617ca4e
MatheusGorga <matheusgobar@gmail.com>
2021-08-24 03:58:52 -0300

    dtmoney

---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,65 @@
+import Modal from 'react-modal';
+import { Container, RadioBox, TransactionTypeContainer } from './styles';
+import closeImg from '../../assets/close.svg'
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
+import { useState } from 'react';
+
+
+
+interface NewTransactionModalProps{
+    isOpen: boolean;
+    onRequestClose: () => void
+}
+
+
+export function NewTransactionModal({isOpen, onRequestClose} : NewTransactionModalProps){
+    const [type, setType] = useState('deposit');
+
+
+    return(
+        <Modal 
+        isOpen={isOpen} 
+        onRequestClose={onRequestClose}
+        overlayClassName="react-modal-overlay"
+        className="react-modal-content"
+        >
+
+        <button  onClick={onRequestClose}  type="button" className="react-modal-close">
+            <img src={closeImg} alt="Fechar Modal" /> 
+        </button>
+
+        <Container>
+            <h2>Cadastrar transação</h2>
+            <input type="text" placeholder="Titulo" />
+            <input type="number" placeholder="Valor" />
+
+            <TransactionTypeContainer>
+                <RadioBox
+                type="button"
+                onClick={() => { setType('deposit');}}
+                isActive={type === 'deposit'}
+                >
+                    <img src={incomeImg} alt="Entrada" />
+                    <span>Entrada</span>
+                </RadioBox>
+
+                <RadioBox
+                type="button"
+                onClick={() => { setType('withdraw');}}
+                isActive={type === 'withdraw'}
+                >
+                    <img src={outcomeImg} alt="Saida" />
+                    <span>Saida</span>
+                </RadioBox>
+            </TransactionTypeContainer>
+
+            <input type="text" placeholder="Categoria" />
+            <button type="submit">Cadastrar</button>
+        </Container>
+       
+
+
+        </Modal>
+    )
+}
---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,40 @@
+import { Container } from "./styles";
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
+import totalImg from '../../assets/total.svg'
+
+
+
+export function Summary(){
+    return(
+        <Container>
+            <div>
+                <header>
+                    <p>Entradas</p>
+                    <img src={incomeImg} alt="entradas" />
+                </header>
+                <strong>
+                    R$2500,00
+                </strong>
+            </div>
+            <div>
+                <header>
+                    <p>Saidas</p>
+                    <img src={outcomeImg} alt="Saidas" />
+                </header>
+                <strong>
+                   - R$500,00
+                </strong>
+            </div>
+             <div className="highlitg">
+                <header>
+                    <p>Total</p>
+                    <img src={totalImg} alt="Total" />
+                </header>
+                <strong>
+                    R$2000,00
+                </strong>
+            </div>
+        </Container>
+    )
+}
